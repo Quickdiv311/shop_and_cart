46c8f4a7e137f51a2562d106869f7fa6769dc907
Divyansh Agarwal <dagarwal058@gmail.com>
2023-08-11 18:21:30 +0530

    header styles

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import styles from './Header.css';
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <div className="header">
+    <div className={styles.header}>
 
-      <div className="left-bar">
-            <span className="shop-container">
-            <i class="bi bi-shop-window" className={styles.shop} style={{fontSize: "25px"}}></i>
-            </span>
-        <span className="title">
-            <h5>SHOP n CART</h5>
+      <div className={styles.leftBar}>
+            <i class="bi bi-shop-window" className={styles.shop} style={{fontSize: "25px",marginRight: "20px",marginLeft: "10px"}}></i>
+        <span className={styles.title}>
+            <h5>SHOP&CART</h5>
         </span>
         </div>
 
-        <div className="right-bar">
-            <span className="cart-container right-barc">
-                <span className="num">1</span>
-                <i class="bi bi-cart" style={{fontSize: "29px"}}></i>
+        <div className={styles.rightBar}>
+            <span className={styles.cartContainer}>
+                <span className={styles.num}>1</span>
+                <i class="bi bi-cart" style={{fontSize: "30px"}}></i>
             </span>
-            <span className="profile right-barc">Sign In</span>
-            <span className="profile-container">
+            <span className={styles.profile}>Sign In</span>
+            <span className={styles.profileContainer}>
                 <i class="bi bi-person-fill" style={{fontSize: "25px"}}></i>
             </span>
         </div>
